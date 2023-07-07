7f58014fa3315f4e4b4eea7760b9f77cd8b6be64
shahin2525 <shahin451999@gmail.com>
2023-07-07 15:04:58 -0700

    login added in navbar

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 // import logo from "../../../assets/logo.png";
 import { AuthContext } from "../../../provider/AuthProvider";
-// import { AuthContext } from "../../provider/AuthProvider";
 
 const Navbar = () => {
   const { logOut, user } = useContext(AuthContext);
@@ -22,11 +21,11 @@ const Navbar = () => {
     <div className="navbar bg-rose-100 flex-col md:flex-row bold">
       <div className="">
         <a className="btn btn-ghost normal-case text-xl">
-          {/* <img src={logo} /> */}
           <img
             height={"100"}
             width={"100"}
-            src="https://i.ibb.co/d4YLVsk/Logo-Makr-9-Wm9-Cv.png"
+            // src="https://i.ibb.co/d4YLVsk/Logo-Makr-9-Wm9-Cv.png"
+            src="https://i.ibb.co/pxbZhhW/toyLogo.png"
           />
         </a>
       </div>
